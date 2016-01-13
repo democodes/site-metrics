baf61b0884542fb71113fbf7e68274ecfeae1ed6
rejuljames <rjames5@hawk.iit.edu>
2016-01-12 16:07:37 -0800

    updated

--- jsontest.py
+++ jsontest.py
@@ -4,5 +4,5 @@ from pprint import pprint
 with open('result.json') as data_file:    
     data = json.load(data_file)
 
-#pprint(data)
-print data["http://facebook.com"][0]
+pprint(data)
+print (data["http://facebook.com"][0])
